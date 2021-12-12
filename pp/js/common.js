dd0c08e3e90c96bccc51426d493cba3a1f8a1c39
Jaeyong Choi <lkmdl2021@gmail.com>
2021-12-13 00:55:31 +0900

    add

---
+++ pp/js/common.js
@@ -0,0 +1,15 @@
+function menu() { /* 함수생성 */
+
+  // 1. 함수호출
+  // console.log("메뉴 호출"); 
+
+  // 2. 대상선택
+  var nav = document.
+  querySelector("#nav");
+  // console.log("선택")
+  
+  // 3. 변경내용
+  nav.classList.toggle("on")
+  // console.log("on/off")
+
+}
