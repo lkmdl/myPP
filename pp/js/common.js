c97ede53315b3093ca952e53acd29902aeeb50a3
lkmdl <lkmdl2021@gmail.com>
2021-12-14 18:24:00 +0900

    add

--- pp/js/common.js
+++ pp/js/common.js
@@ -1,15 +1,8 @@
-function menu() { /* 함수생성 */
+function menu() {
 
-  // 1. 함수호출
-  // console.log("메뉴 호출"); 
-
-  // 2. 대상선택
   var top = document.
   querySelector("#top");
-  // console.log("선택")
   
-  // 3. 변경내용
   top.classList.toggle("on")
-  // console.log("on/off")
 
 }
